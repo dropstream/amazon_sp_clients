ad5e8002c14a88f7ce6ec6fa91a266e03272c55d
Ernest <ests@users.noreply.github.com>
2021-05-29 17:52:29 +0200

    Remove invalid code

--- lib/amazon_sp_clients.rb
+++ lib/amazon_sp_clients.rb
@@ -1,17 +1,11 @@
 require 'amazon_sp_clients/version'
+
 require 'amazon_sp_clients/api_client'
 require 'amazon_sp_clients/api_error'
 require 'amazon_sp_clients/configuration'
-require 'amazon_sp_clients/version'
-require 'amazon_sp_clients/version'
 
 module AmazonSpClients
   class << self
-    AmazonSpClients.configure do |config|
-      config.username = 'xxx'
-      config.password = 'xxx'
-    end
-
     # If no block given, return the default Configuration object.
     def configure
       if block_given?
