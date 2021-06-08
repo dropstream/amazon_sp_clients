2bfd36f3ea1c4101d440d3f8c0782341992946d8
Ernest <ests@users.noreply.github.com>
2021-06-08 17:03:39 +0200

    Spec cleanup

--- spec/amazon_sp_clients_spec.rb
+++ spec/amazon_sp_clients_spec.rb
@@ -25,20 +25,7 @@ RSpec.describe AmazonSpClients do
         stub_request(
           :get,
           'https://sandbox.sellingpartnerapi-na.amazon.com/orders/v0/orders?CreatedAfter=TEST_CASE_200&MarketplaceIds=ATVPDKIKX0DER'
-        ).to_return(status: 200, body: fixture('orders_200_response'))
-
-        # .with(
-        #   headers: {
-        #     'Accept' => 'application/json',
-        #     'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        #     'Authorization' => 'AWS4-HMAC-SHA256 Credential=FooBar/20210603/us-east-1/execute-api/aws4_request, SignedHeaders=accept;content-type;host;user-agent;x-amz-access-token;x-amz-date, Signature=d4a72b2c79678336713c3d931d9610580d20edff69d7a80bbc2e2cbda06e9974',
-        #     'Content-Type' => 'application/json',
-        #     'Host' => 'sandbox.sellingpartnerapi-na.amazon.com',
-        #     'User-Agent' => 'Dropstream/1.0 (Language=Ruby/2.5.8)',
-        #     'X-Amz-Access-Token' => 'Azta|FooBar',
-        #     'X-Amz-Date' => '20210603T105955Z'
-        #   }
-        # )
+        ).to_return(status: 200, body: fixture('orders_200_response.json'))
 
         orders_api = AmazonSpClients::SpOrdersV0::OrdersV0Api.new
         get_orders_response =
@@ -59,7 +46,7 @@ RSpec.describe AmazonSpClients do
           get_orders_response.payload
         )
 
-        expect(orders.orders.first.order_status).to eq 'Pending'
+        expect(orders.orders.first[:OrderStatus]).to eq 'Pending'
       end
     end
   end
