82dd3d0de191e2f2515e4f74a03f8ee370feca34
Ernest <ests@users.noreply.github.com>
2021-07-14 12:41:43 +0200

    Fix main spec

--- spec/amazon_sp_clients_spec.rb
+++ spec/amazon_sp_clients_spec.rb
@@ -4,15 +4,36 @@ require 'logger'
 require 'dotenv/load'
 require 'awesome_print'
 require 'timecop'
+require 'ostruct'
 require 'pry-byebug'
 
 require 'amazon_sp_clients/sp_orders_v0'
 require 'amazon_sp_clients/sp_tokens_2021'
 
+class NullSession
+  def authenticate(*args); end
+
+  def refresh; end
+
+  def ask_for_restricted_data_token; end
+
+  def access_token
+    'oaisdhgoajsdfoahgasd'
+  end
+
+  def role_credentials
+    OpenStruct.new({})
+  end
+
+  def restricted_data_token
+    'RESTRTOKENosdfjaoighasdf'
+  end
+end
+
 RSpec.describe AmazonSpClients do
   before do
-    # new_time = Time.local(2018, 9, 1, 12, 0, 0)
-    # Timecop.freeze(new_time)
+    new_time = Time.local(2018, 9, 1, 12, 0, 0)
+    Timecop.freeze(new_time)
 
     AmazonSpClients.configure do |c|
       c.access_key = ENV['AMZ_ACCESS_KEY_ID'] || 'ACCESS_KEY'
@@ -22,7 +43,7 @@ RSpec.describe AmazonSpClients do
       c.client_id = ENV['AMZ_CLIENT_ID'] || 'CLIENT_ID'
       c.client_secret = ENV['AMZ_CLIENT_SECRET'] || 'CLIENT_SECRET'
 
-      # c.sandbox_env!
+      c.sandbox_env!
       c.logger = Logger.new($stdout)
       c.logger.level = Logger::DEBUG
       c.debugging = true
@@ -31,12 +52,27 @@ RSpec.describe AmazonSpClients do
 
   describe 'restricted access resources' do
     context 'success path' do
-      it 'returns success response with PII data' do 
-        pending
-        session, err = AmazonSpClients.new_session(ENV['AMZ_REFRESH_TOKEN']) 
-        orders_api = AmazonSpClients::SpOrdersV0::OrdersV0Api.new(session)
-        addr_resp = orders_api.get_order_address('113-1435144-7135426', auth_names: [:pii])
+      it 'returns success response with PII data' do
+        stub_request(:post, 'https://sts.amazonaws.com/').to_return(
+          status: 200,
+          body: fixture('sts_200_response.xml'),
+        )
 
+        stub_request(:post, 'https://api.amazon.com/auth/o2/token').to_return(
+          status: 200,
+          body: fixture('token_success.json'),
+        )
+        stub_request(
+          :get,
+          'https://sandbox.sellingpartnerapi-na.amazon.com/orders/v0/orders/113-1435144-7135426/address',
+        ).to_return(status: 200, body: '{"payload": {}}', headers: {})
+        orders_api =
+          AmazonSpClients::SpOrdersV0::OrdersV0Api.new(NullSession.new)
+        addr_resp =
+          orders_api.get_order_address(
+            '113-1435144-7135426',
+            auth_names: [:pii],
+          )
         expect(addr_resp.payload).not_to be_nil
       end
     end
@@ -45,40 +81,34 @@ RSpec.describe AmazonSpClients do
   describe 'complete flow test' do
     context 'success path' do
       it 'returns success responses' do
-        pending
-        stub_request(:post, 'https://sts.amazonaws.com/')
-          .to_return(status: 200, body: fixture('sts_200_response.xml'))
+        stub_request(:post, 'https://sts.amazonaws.com/').to_return(
+          status: 200,
+          body: fixture('sts_200_response.xml'),
+        )
 
-        stub_request(:post, 'https://api.amazon.com/auth/o2/token')
-          .to_return(status: 200, body: fixture('token_success.json'))
+        stub_request(:post, 'https://api.amazon.com/auth/o2/token').to_return(
+          status: 200,
+          body: fixture('token_success.json'),
+        )
 
         stub_request(
-            :get,
-            'https://sandbox.sellingpartnerapi-na.amazon.com/orders/v0/orders?CreatedAfter=TEST_CASE_200&MarketplaceIds=ATVPDKIKX0DER'
-          )
-          .to_return(status: 200, body: fixture('orders_200_response.json'))
+          :get,
+          'https://sandbox.sellingpartnerapi-na.amazon.com/orders/v0/orders?CreatedAfter=TEST_CASE_200&MarketplaceIds=ATVPDKIKX0DER',
+        ).to_return(status: 200, body: fixture('orders_200_response.json'))
 
         refresh_token = ENV['AMZ_REFRESH_TOKEN'] || 'REFRESH_TOKEN'
 
-        get_orders_response = nil
-        session_err = AmazonSpClients.new_session(refresh_token) do |session|
-          orders_api = AmazonSpClients::OrdersV0Api.new(session)
-          get_orders_response =
-            orders_api.get_orders(
-              ['ATVPDKIKX0DER'],
-              created_after: 'TEST_CASE_200'
-            )
-          orders_api = AmazonSpClients::OrdersV0Api.new(session)
-          get_orders_response =
-            orders_api.get_orders(
-              ['ATVPDKIKX0DER'],
-              created_after: 'TEST_CASE_200'
-            )
-        end
+        session, err = AmazonSpClients.new_session(refresh_token)
+        orders_api = AmazonSpClients::SpOrdersV0::OrdersV0Api.new(session)
+        get_orders_response =
+          orders_api.get_orders(
+            ['ATVPDKIKX0DER'],
+            created_after: 'TEST_CASE_200',
+          )
 
-        expect(session_err).to be_nil
+        expect(err).to be_nil
         expect(get_orders_response).to be_instance_of(
-          AmazonSpClients::ApiResponse
+          AmazonSpClients::ApiResponse,
         )
         expect(get_orders_response.payload).to be_a(Hash)
         expect(get_orders_response.payload[:Orders].first).to be_a(Hash)
@@ -98,57 +128,51 @@ RSpec.describe AmazonSpClients do
 
         refresh_token = ENV['AMZ_REFRESH_TOKEN'] || 'REFRESH_TOKEN'
 
-        yielded = false
-        session_err = AmazonSpClients.new_session(refresh_token) do |session|
-          yielded = true
-        end
+        _, err = AmazonSpClients.new_session(refresh_token)
 
-        expect(session_err).not_to be_nil
-        expect(session_err.error).to eq "Sender: SignatureDoesNotMatch"
-        expect(session_err.message).to match /The request signature we calculated does.+/
-        expect(yielded).to eq false
+        expect(err).not_to be_nil
+        expect(err.error).to eq 'Sender: SignatureDoesNotMatch'
+        expect(err.message).to match /The request signature we calculated does.+/
       end
     end
 
     context 'with token error' do
       it 'session never runs and returns error' do
-        pending
-        stub_request(:post, 'https://sts.amazonaws.com/')
-          .to_return(status: 200, body: fixture('sts_200_response.xml'))
+        stub_request(:post, 'https://sts.amazonaws.com/').to_return(
+          status: 200,
+          body: fixture('sts_200_response.xml'),
+        )
 
-        stub_request(:post, 'https://api.amazon.com/auth/o2/token')
-          .to_return(status: 400, body: fixture('token_error.json'))
+        stub_request(:post, 'https://api.amazon.com/auth/o2/token').to_return(
+          status: 400,
+          body: fixture('token_error.json'),
+        )
 
         refresh_token = ENV['AMZ_REFRESH_TOKEN'] || 'REFRESH_TOKEN'
 
-        yielded = false
-        session_err = AmazonSpClients.new_session(refresh_token) do |session|
-          yielded = true
-        end
+        session, err = AmazonSpClients.new_session(refresh_token)
 
-        expect(session_err).not_to be_nil
-        expect(session_err.error).to eq "invalid_client"
-        expect(session_err.message).to eq "Client authentication failed"
-        expect(yielded).to eq false
+        expect(session).to be_nil
+        expect(err).not_to be_nil
+        expect(err.error).to eq 'invalid_client'
+        expect(err.message).to eq 'Client authentication failed'
       end
     end
 
     describe 'middlewares' do
       it 'allows hooking into request/response env' do
-        pending
-        stub_request(:post, 'https://sts.amazonaws.com/')
-          .to_return(status: 200, body: fixture('sts_200_response.xml'))
-        stub_request(:post, 'https://api.amazon.com/auth/o2/token')
-          .to_return(status: 200, body: fixture('token_success.json'))
+        stub_request(:post, 'https://sts.amazonaws.com/').to_return(
+          status: 200,
+          body: fixture('sts_200_response.xml'),
+        )
+        stub_request(:post, 'https://api.amazon.com/auth/o2/token').to_return(
+          status: 200,
+          body: fixture('token_success.json'),
+        )
         stub_request(
           :get,
-          'https://sandbox.sellingpartnerapi-na.amazon.com/orders/v0/orders?CreatedAfter=TEST_CASE_200&MarketplaceIds=ATVPDKIKX0DER'
+          'https://sandbox.sellingpartnerapi-na.amazon.com/orders/v0/orders?CreatedAfter=TEST_CASE_200&MarketplaceIds=ATVPDKIKX0DER',
         ).to_return(status: 200, body: fixture('orders_200_response.json'))
-
-        # AmazonSpClients.on_request do |req|
-        #   method = req[:method]
-        #   params = req[:params]
-        # end
 
         method = nil
         api_call_opts = nil
@@ -161,27 +185,27 @@ RSpec.describe AmazonSpClients do
 
         refresh_token = ENV['AMZ_REFRESH_TOKEN'] || 'REFRESH_TOKEN'
 
-        session_err = AmazonSpClients.new_session(refresh_token) do |session|
-          orders_api = AmazonSpClients::OrdersV0Api.new(session)
-          get_orders_response =
-            orders_api.get_orders(
-              ['ATVPDKIKX0DER'],
-              created_after: 'TEST_CASE_200'
-            )
-        end
+        session, _ = AmazonSpClients.new_session(refresh_token)
+        orders_api = AmazonSpClients::SpOrdersV0::OrdersV0Api.new(session)
+        get_orders_response =
+          orders_api.get_orders(
+            ['ATVPDKIKX0DER'],
+            created_after: 'TEST_CASE_200',
+          )
 
         expect(method).to eq :get
         expect(status).to eq 200
-        expect(api_call_opts[:query_params][:CreatedAfter]).to eq 'TEST_CASE_200'
+        expect(
+          api_call_opts[:query_params][:CreatedAfter],
+        ).to eq 'TEST_CASE_200'
       end
     end
 
     context 'error api response' do
       it 'returns error response' do
-        pending
         stub_request(
           :get,
-          'https://sandbox.sellingpartnerapi-na.amazon.com/orders/v0/orders?CreatedAfter=TEST_CASE_400&MarketplaceIds=ATVPDKIKX0DER'
+          'https://sandbox.sellingpartnerapi-na.amazon.com/orders/v0/orders?CreatedAfter=TEST_CASE_400&MarketplaceIds=ATVPDKIKX0DER',
         ).to_return(
           status: 400,
           body:
@@ -194,24 +218,25 @@ RSpec.describe AmazonSpClients do
             'x-amzn-RequestId' => '3b9f0d8b-0b92-4582-8152-a5c56b5c998d',
             'x-amz-apigw-id' => 'ApoJKGnsIAMF7Xw=',
             'X-Amzn-Trace-Id' =>
-              'Root=1-60c08707-4aade6f26fc77d032b0ccefe;Sampled=0'
-          }
+              'Root=1-60c08707-4aade6f26fc77d032b0ccefe;Sampled=0',
+          },
         )
 
-        orders_api = AmazonSpClients::SpOrdersV0::OrdersV0Api.new
+        orders_api =
+          AmazonSpClients::SpOrdersV0::OrdersV0Api.new(NullSession.new)
         get_orders_response =
           orders_api.get_orders(
             ['ATVPDKIKX0DER'],
-            created_after: 'TEST_CASE_400'
+            created_after: 'TEST_CASE_400',
           )
 
         expect(get_orders_response.errors).not_to be_nil
         expect(get_orders_response.payload).to be_nil
         expect(get_orders_response.errors).to be_instance_of(
-          AmazonSpClients::ApiError
+          AmazonSpClients::ApiError,
         )
         expect(get_orders_response.errors.full_messages).to eq(
-          'InvalidInput: Invalid Input'
+          'InvalidInput: Invalid Input',
         )
 
         expect(get_orders_response.original_response.status).to eq 400
