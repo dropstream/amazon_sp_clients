f47f81a4559a229581eecea467a5ce4d3ad83259
Ernest <ests@users.noreply.github.com>
2022-03-30 15:05:45 -0400

    There's no need to symbolize keys (JSON parser already does it)

--- lib/amazon_sp_clients/api_response.rb
+++ lib/amazon_sp_clients/api_response.rb
@@ -19,13 +19,11 @@ module AmazonSpClients
 
       if attributes.key?('payload') || attributes.key?(:payload) || attributes.key?('errors') ||
            attributes.key?(:errors)
-        attributes.transform_keys! { |k| k.to_sym }
         self.payload = attributes[:payload]
         if attributes.key?(:errors)
           self.errors = AmazonSpClients::ApiError.new(attributes.delete(:'errors'))
         end
       else
-        attributes.transform_keys! { |k| underscore(k).to_sym }
         self.payload = attributes
       end
     end
@@ -35,21 +33,6 @@ module AmazonSpClients
     # @return [Object] Returns the model itself
     def self.build_from_hash(attributes, response = nil)
       new(attributes, response)
-    end
-
-    private
-
-    # doesn't work with acronyms
-    def underscore(camel_cased_word)
-      return camel_cased_word unless /[A-Z-]|::/.match?(camel_cased_word)
-      word = camel_cased_word.to_s.gsub('::', '/')
-
-      # word.gsub!(inflections.acronyms_underscore_regex) { "#{$1 && '_' }#{$2.downcase}" }
-      word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
-      word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
-      word.tr!('-', '_')
-      word.downcase!
-      word
     end
   end
 end
--- spec/amazon_sp_clients/api_response_spec.rb
+++ spec/amazon_sp_clients/api_response_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe AmazonSpClients::ApiResponse do
   subject(:api_response) { described_class }
 
   describe '.build_from_hash' do
-    it 'underscores first level of response hash' do
-      input = { 'payload' => { BogusField: { MWSAuthToken: 'test' } } }
+    it 'set payload attr at success response body' do
+      input = { payload: { BogusField: { MWSAuthToken: 'test' } } }
       expected = { BogusField: { MWSAuthToken: 'test' } }
       response = api_response.build_from_hash(input)
       expect(response.payload).to eq(expected)
     end
 
-    it 'wraps errors attribute with ApiError' do
+    it 'wraps single error with ApiError' do
       input = {"errors": [{"code": "InvalidInput", "message": "Invalid Input"}]}
       response = api_response.build_from_hash(input)
       errors = response.errors
@@ -19,9 +19,9 @@ RSpec.describe AmazonSpClients::ApiResponse do
       expect(errors.errors.first.code).to eq "InvalidInput"
     end
 
-    it 'always wraps non-erro attributes with payload' do
+    it 'always sets payload attr on no errors' do
       input = { feedId: 1, ResultDocumentId: 'test' }
-      expected = { feed_id: 1, result_document_id: 'test' }
+      expected = { feedId: 1, ResultDocumentId: 'test' }
       response = api_response.build_from_hash(input)
 
       expect(response.payload).to eq(expected)
